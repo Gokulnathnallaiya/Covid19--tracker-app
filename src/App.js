472329f5ec71c0d4b678f3203353fa9089364836
gokul nath <gokulaps1998@gmail.com>
2020-06-20 20:45:13 +0530

    completed cards part

--- src/App.js
+++ src/App.js
@@ -4,18 +4,23 @@ import {Cards, Chart, CountryPicker} from "./components"
 import styles from './App.module.css';
 import { fetchData } from "./api";
 class App extends React.Component{
+    state={
+        data:{},
+        country:'',
+    }
 
     async componentDidMount(){
-        const data = await fetchData();
-        console.log(data)
+        const fetchedData = await fetchData();
+        this.setState({data : fetchedData.data})
 
     }
     render(){
+        const {data}=this.state
         return(
             <div className={styles.container}>
-                <Cards/>
-                <Chart/>
-                <CountryPicker/>
+                <Cards data={data}/>
+                {/* <Chart/>
+                <CountryPicker/> */}
             </div>
 
         )
