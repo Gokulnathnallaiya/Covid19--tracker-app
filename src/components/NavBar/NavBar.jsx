0e3b75ef504c34e2a3082f918708c4a1360551cc
gokul nath <gokulaps1998@gmail.com>
2020-07-10 07:47:58 +0530

    navbar modified

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,6 +9,8 @@ const navBar = () => {
                 
                     <img className={styles.logo} src={logo} alt="logo" />
                     <h1 className={styles.navbar} >COVID TRACKER</h1>
+                    <a className={styles.githublink} href="https://github.com/Gokulnathnallaiya/Covid19--tracker-app">Contact Developer</a>
+
                     <a className={styles.githublink} href="https://github.com/Gokulnathnallaiya/Covid19--tracker-app">Github Repo</a>
 
                 
